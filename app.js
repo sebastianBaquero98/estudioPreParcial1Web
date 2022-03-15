8b85b757d99e3dea46f030f622b888e06f938f4a
sebastianBaquero98 <s.baqueroa@uniandes.edu.co>
2022-03-15 10:18:26 -0500

    First Commit

---
+++ app.js
@@ -0,0 +1,57 @@
+let sortDirection = false;
+ejemploJSON = [{"last_name": "Zaws",
+                "first_name":"Nari",
+                "email":"zdaw@deli.com",
+                "photo":"http://aummyimage.com/155x119.jpg/ff4444/ffffff"},
+
+                {"last_name": "Aithams",
+                "first_name":"Winona",
+                "email":"adaw@deli.com",
+                "photo":"http://bummyimage.com/155x119.jpg/ff4444/ffffff"},
+
+                {"last_name": "rtreatfield",
+                "first_name":"Northup",
+                "email":"cdaw@deli.com",
+                "photo":"http://cummyimage.com/155x119.jpg/ff4444/ffffff"}
+                ]
+
+function cargarDatos(datos){
+    
+    tbody = document.getElementById('table-body')
+    datos.forEach(person => {
+        console.log(person)
+        let row = document.createElement('tr');
+    
+        /* Last Name */
+        let last_name = document.createElement('td');
+        last_name.innerHTML = person.last_name;
+        row.appendChild(last_name);
+    
+        /* First Name */
+        let first_name = document.createElement('td');
+        first_name.innerHTML = person.first_name;
+        row.appendChild(first_name);
+    
+        /* Email */
+        let email = document.createElement('td');
+        email.innerHTML = person.email;
+        row.appendChild(email);
+    
+        /* Photo */
+        let photo = document.createElement('td');
+        photo.innerHTML = person.photo;
+        row.appendChild(photo);
+    
+        tbody.appendChild(row);
+    })
+}
+
+
+function sortColumn(columnName){
+    sortDirection = !sortDirection;
+
+    ejemploJSON.sort((a, b) =>
+        a[columnName].toLowerCase().localeCompare(b[columnName].toLowerCase())
+    );
+    cargarDatos(ejemploJSON);
+}
